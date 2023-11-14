c5ba7d0a7427440bf5a884bcf38f38767d0a9a64
Durgesh kumar prajapati <98798977+Durgesh4993@users.noreply.github.com>
2023-11-14 07:25:52 +0530

    Update pixie-integrate.js

--- pixie-integrate.js
+++ pixie-integrate.js
@@ -6,8 +6,6 @@
     Pixie = function() {
 
         _pixie.start();
-        
-
         instance = {
             save: function(format, quality) { _pixie.save(format, quality); return this },
             getService: function(name) { return _pixie.getService(name); },
